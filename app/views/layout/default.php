8e999d216123d249b4dce7a997aace3b647f4bee
drozddmitriy <drozd.dmitriy.etc@gmail.com>
2019-04-12 00:57:32 +0300

    footer

--- app/views/layout/default.php
+++ app/views/layout/default.php
@@ -6,6 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="/public/styles/bootstrap.css" rel="stylesheet">
+    <link rel="stylesheet" href="/public/styles/footer.css">
     <script type="text/javascript" src="/public/scripts/jquery-3.4.0.min.js"></script>
     <script type="text/javascript" src="/public/scripts/popper.min.js"></script>
     <script type="text/javascript" src="/public/scripts/bootstrap.js"></script>
@@ -30,16 +31,21 @@
         <?php endif; ?>
     </div>
 </div>
-<div class="container">
+<div class="container content">
     <div class="row justify-content-center">
-        <div class="col-7">
+        <div class="content-inside col-7">
 
             <?php echo $content; ?>
 
-            <div style="height: 20px"></div>
         </div>
     </div>
 </div>
+
+<footer class="footer page-footer font-small blue">
+    <div class="footer-copyright text-center py-3">© 2019p Copyright:
+        <a href="#">Bootstrap.com</a>
+    </div>
+</footer>
 
 </body>
 </html>
